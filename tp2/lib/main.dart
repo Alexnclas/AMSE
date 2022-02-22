b895b46b391be5c00fca6edc0a3da6d8b3ec374e
Alex <alex.nicolas@etu.imt-nord-europe.fr>
2022-02-22 15:09:01 +0100

    exercice 4

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -25,6 +25,7 @@ class ListeExos extends StatelessWidget {
         children: [
           Exo1(),
           Exo2(),
+          Exo4(),
         ],
       )
     );
@@ -145,6 +146,63 @@ class Exo2 extends StatelessWidget {//Exercice 2
   }
 }
 
+
+//../images/parliamentMothershipConnection.jpg
+class Exo4 extends StatelessWidget {//Exercice 1
+  const Exo4({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+            padding: const EdgeInsets.all(16),
+            child: Card(
+              clipBehavior: Clip.antiAlias,
+              child: GestureDetector(
+                onTap: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context){
+                        //CONTENU EXERCICE
+                        return Scaffold(
+                          appBar: AppBar(
+                            title: Text("Exercice 4: Affichage d'une tuile (un morceau d'image)"),
+                          ),
+                          body: Center(
+                            child: ClipRect(
+                              child: Container(
+                                child: Align(
+                                  alignment: Alignment(0,0),
+                                  widthFactor: 0.3,
+                                  heightFactor: 0.3,
+                                  child: Image.network('../images/parliamentMothershipConnection.jpg'),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }
+                    )
+                  );
+                },
+                //FIN CONTENU EXERCICE
+                //CONTENU CARTE LISTE EXERCICE
+                child: ListTile(
+                  title: Text("Exercice 4"),
+                  subtitle: Text(
+                    "Affichage d'une tuile (un morceau d'image)",
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward,
+                    color: Colors.blue,
+                    semanticLabel: "Lancer l'exercice",
+                  ),
+                ),
+              ),
+            ),
+          );
+  }
+}
 
 
 
@@ -288,3 +346,10 @@ class _MyAnimateButtonState extends State<MyAnimateButton>{
           );
   }
 }
+
+
+
+
+
+//CLASSES EXERCICE 4
+/*  */
