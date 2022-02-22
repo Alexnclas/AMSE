455645738c3b3f564ba6047378e48256783188a2
Alex <alex.nicolas@etu.imt-nord-europe.fr>
2022-02-22 16:17:18 +0100

    Exercice 5b

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -27,6 +27,7 @@ class ListeExos extends StatelessWidget {
           Exo2(),
           Exo4(),
           Exo5a(),
+          Exo5b(),
         ],
       )
     );
@@ -34,7 +35,7 @@ class ListeExos extends StatelessWidget {
 }
 
 
-class Exo1 extends StatelessWidget {//Exercice 1
+class Exo1 extends StatelessWidget {
   const Exo1({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -148,8 +149,7 @@ class Exo2 extends StatelessWidget {//Exercice 2
 }
 
 
-//../images/parliamentMothershipConnection.jpg
-class Exo4 extends StatelessWidget {//Exercice 1
+class Exo4 extends StatelessWidget {
   const Exo4({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -208,7 +208,7 @@ class Exo4 extends StatelessWidget {//Exercice 1
 
 
 
-class Exo5a extends StatelessWidget {//Exercice 1
+class Exo5a extends StatelessWidget {
   const Exo5a({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -301,6 +301,177 @@ class Exo5a extends StatelessWidget {//Exercice 1
                   title: Text("Exercice 5a"),
                   subtitle: Text(
                     "Génération d'un plateau de tuiles",
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward,
+                    color: Colors.blue,
+                    semanticLabel: "Lancer l'exercice",
+                  ),
+                ),
+              ),
+            ),
+          );
+    }
+}
+
+//../images/parliamentMothershipConnection.jpg
+class Exo5b extends StatelessWidget {
+  const Exo5b({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+            padding: const EdgeInsets.all(16),
+            child: Card(
+              clipBehavior: Clip.antiAlias,
+              child: GestureDetector(
+                onTap: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context){
+                        //CONTENU EXERCICE
+                        return Scaffold(
+                          appBar: AppBar(
+                            title: Text("Exercice 5b: Grille (Image)"),
+                          ),
+                          body: Center(
+                            child: GridView.count(
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10,
+                              crossAxisCount: 3,
+                              children:[
+                                FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: ClipRect(
+                                    child: Container(
+                                      child: Align(
+                                        alignment: Alignment(-1,-1),
+                                        widthFactor: 0.3,
+                                        heightFactor: 0.3,
+                                        child: Image.network('../images/parliamentMothershipConnection.jpg'),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: ClipRect(
+                                    child: Container(
+                                      child: Align(
+                                        alignment: Alignment(0,-1),
+                                        widthFactor: 0.3,
+                                        heightFactor: 0.3,
+                                        child: Image.network('../images/parliamentMothershipConnection.jpg'),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: ClipRect(
+                                    child: Container(
+                                      child: Align(
+                                        alignment: Alignment(1,-1),
+                                        widthFactor: 0.3,
+                                        heightFactor: 0.3,
+                                        child: Image.network('../images/parliamentMothershipConnection.jpg'),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: ClipRect(
+                                    child: Container(
+                                      child: Align(
+                                        alignment: Alignment(-1, 0),
+                                        widthFactor: 0.3,
+                                        heightFactor: 0.3,
+                                        child: Image.network('../images/parliamentMothershipConnection.jpg'),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: ClipRect(
+                                    child: Container(
+                                      child: Align(
+                                        alignment: Alignment(0, 0),
+                                        widthFactor: 0.3,
+                                        heightFactor: 0.3,
+                                        child: Image.network('../images/parliamentMothershipConnection.jpg'),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: ClipRect(
+                                    child: Container(
+                                      child: Align(
+                                        alignment: Alignment(1,0),
+                                        widthFactor: 0.3,
+                                        heightFactor: 0.3,
+                                        child: Image.network('../images/parliamentMothershipConnection.jpg'),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: ClipRect(
+                                    child: Container(
+                                      child: Align(
+                                        alignment: Alignment(-1, 1),
+                                        widthFactor: 0.3,
+                                        heightFactor: 0.3,
+                                        child: Image.network('../images/parliamentMothershipConnection.jpg'),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: ClipRect(
+                                    child: Container(
+                                      child: Align(
+                                        alignment: Alignment(0, 1),
+                                        widthFactor: 0.3,
+                                        heightFactor: 0.3,
+                                        child: Image.network('../images/parliamentMothershipConnection.jpg'),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: ClipRect(
+                                    child: Container(
+                                      child: Align(
+                                        alignment: Alignment(1, 1),
+                                        widthFactor: 0.3,
+                                        heightFactor: 0.3,
+                                        child: Image.network('../images/parliamentMothershipConnection.jpg'),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+                    )
+                  );
+                },
+                //FIN CONTENU EXERCICE
+                //CONTENU CARTE LISTE EXERCICE
+                child: ListTile(
+                  title: Text("Exercice 5b"),
+                  subtitle: Text(
+                    "Génération d'un plateau de tuiles (Image)",
                     style: TextStyle(color: Colors.black.withOpacity(0.6)),
                   ),
                   trailing: Icon(
